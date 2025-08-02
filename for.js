7af13348310377b3bdd01b65182e820fabd5708a
Aarzuna <aarjustarlook@hotmail.com>
2025-08-02 23:05:40 +0545

    static data

--- for.js
+++ for.js
@@ -4,3 +4,15 @@ window.onload = function () {
       event.preventDefault();
       return;
     }
+     event.preventDefault();
+
+    // Collect static form data
+    const fullName = document.getElementById("fullName").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const address = document.getElementById("address").value;
+    const links = document.getElementById("links").value;
+    const careerObjective = document.getElementById("careerObjective").value;
+    const certifications = document.getElementById("certifications").value;
+    const projects = document.getElementById("projects").value;
+    const references = document.getElementById("references").value;
